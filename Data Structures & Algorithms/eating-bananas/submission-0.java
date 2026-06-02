0ce2f57ae5089bb5cf8c07c1f5b0546da9b48db5
Ali Kemal Tamkoc <71703855+atamkoc@users.noreply.github.com>
2026-06-02 22:44:50 +0200

    Add: eating-bananas - submission-0

---
+++ Data Structures & Algorithms/eating-bananas/submission-0.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        // piles.length <= h =====> edge case already handled
+        // piles[i] -> banana in that pile
+        // h -> total time we have
+        // k -> total bananas we can eat in a single time quantity
+        // ceil (piles[i] / k) -> gives the time spent while eating the bananas in pile[i]
+        //trying to minimize k
+        Arrays.sort(piles);
+
+        int right = piles[piles.length - 1];
+        int left = 1;
+        int bestSpeed = right;
+
+        while (left<=right){
+            //perform the binary search between right - left
+            int mid = (right + left) / 2;
+            long hourSpent = 0;
+            for(int i = 0; i < piles.length; i++) {
+                hourSpent += (piles[i] + mid - 1) / mid;
+            }
+
+            if (hourSpent > h){
+                left = mid +1;
+            }
+            else{
+                bestSpeed = mid;
+                right = mid -1;
+            }
+        }
+
+
+        return bestSpeed;
+
+    }
+}
